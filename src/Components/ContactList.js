8c23ee94eb0bf653a15765fcb5eca484dc7f136e
Kedar Dhoble <kedardhoble2001@gmail.com>
2024-06-09 23:45:16 +0530

    Refactored the contactList.

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,30 +1,34 @@
 import React from "react";
-import ContactCard from "./ContactCard";
 import { useNavigate } from "react-router-dom";
+import ContactCard from "./ContactCard";
 
-const ContactList = (props) => {
-  console.log(props);
-  const navigate=useNavigate()
+const ContactList = ({ contacts, getContactId }) => {
+  const navigate = useNavigate();
 
-  const deleteConactHandler = (id) => {
-    props.getContactId(id);
+  const deleteContactHandler = (id) => {
+    getContactId(id);
   };
-
- 
 
   return (
     <div className='main'>
-        <h2>
-            Contact List
-            <button className="ui button blue right" onClick={()=>{navigate("/add")}}>Add Contact</button>
-        </h2>
-        <div className="ui celled list">{props.contacts.map((contact) => {
-      return <ContactCard
-        contact={contact}
-        clickHander={deleteConactHandler}
-        key={contact.id}
-      />
-  })}</div>
+      <h2>
+        Contact List
+        <button
+          className="ui button blue right"
+          onClick={() => navigate("/add")}
+        >
+          Add Contact
+        </button>
+      </h2>
+      <div className="ui celled list">
+        {contacts.map((contact) => (
+          <ContactCard
+            key={contact.id}
+            contact={contact}
+            clickHander={deleteContactHandler}
+          />
+        ))}
+      </div>
     </div>
   );
 };
